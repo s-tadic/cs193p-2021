8bab124efbb648c358feb075c9062b245ad8e067
Stefan Tadic <stefan.tadic@gmail.com>
2021-08-05 16:42:07 -0400

    Task 1 : type in lectures 1 and 2

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Stefan Tadic on 8/4/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
